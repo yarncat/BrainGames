f2f0a34e47349fbb094a762704bf4a3f78d627d8
Alexandr Petrov <yarncat@yandex.ru>
2020-07-01 17:54:53 +0400

    fixed linter errors

--- src/games/Progression.php
+++ src/games/Progression.php
@@ -4,11 +4,11 @@ namespace BrainGames\Progression;
 
 function arithmeticProgression($number1, $number2)
 {
-	$array = [];
-	for ($i = 0; $i < 10; $i++) {
-		$number1 = $number1 + $number2;
-		$array[] = $number1;
-	}
+    $array = [];
+    for ($i = 0; $i < 10; $i++) {
+        $number1 = $number1 + $number2;
+        $array[] = $number1;
+    }
     return $array;
 }
 
@@ -19,10 +19,10 @@ function progression()
     for ($i = 1; $i <= ROUNDS; $i++) {
         $number1 = rand(1, 100);
         $number2 = rand(1, 10);
-		$progressionArray = arithmeticProgression($number1, $number2);
-		$lostMemberIndex = array_rand($progressionArray);
-		$result = $progressionArray[$lostMemberIndex];
-		$progressionArray[$lostMemberIndex] = '..';
+        $progressionArray = arithmeticProgression($number1, $number2);
+        $lostMemberIndex = array_rand($progressionArray);
+        $result = $progressionArray[$lostMemberIndex];
+        $progressionArray[$lostMemberIndex] = '..';
         $expression = implode(' ', $progressionArray);
         $answerArray[$i]['expression'] = $expression;
         $answerArray[$i]['result'] = $result;
